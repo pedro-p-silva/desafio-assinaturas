9e225d0a79e71e72bca8ae6b8f95f1226b7f0a47
Pedro Paulo Silva <98651810+p3dr1n80@users.noreply.github.com>
2024-07-28 21:37:26 -0300

    Adicionando o crud faturas e pasta docs

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Function that adds another ten days
+ *
+ * @return string
+ */
 function addAnotherTenDays(): string
 {
     return date('Y-m-d', strtotime('+ 10 days', strtotime(now())));
