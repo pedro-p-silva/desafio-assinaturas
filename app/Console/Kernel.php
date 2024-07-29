2f23bd2be924a6476bf66c791584216a3fc5c31a
Pedro Paulo Silva <98651810+p3dr1n80@users.noreply.github.com>
2024-07-28 23:36:14 -0300

    Ajuste de timezone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:convert_signatures_to_invoices')->everyMinute();
+        $schedule->command('app:convert_signatures_to_invoices')->daily();
     }
 
     /**
